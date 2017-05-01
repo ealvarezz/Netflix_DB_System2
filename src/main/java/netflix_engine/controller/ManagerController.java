4950d8ce975a52f97f782e70609033e98a24ab42
livefree1 <edy.alva_101@hotmail.com>
2017-04-30 23:10:59 -0400

    manager ctrl

--- src/main/java/netflix_engine/controller/ManagerController.java
+++ src/main/java/netflix_engine/controller/ManagerController.java
@@ -1,5 +1,41 @@
 package netflix_engine.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import netflix_engine.model.Employee;
+import netflix_engine.model.FuegoOrder;
+import netflix_engine.model.Movie;
+import netflix_engine.general.Status;
+import netflix_engine.model.Account;
+import netflix_engine.model.Customer;
+import netflix_engine.service.EmployeeService;
+
+@Controller
+@SessionAttributes("Manager")
 public class ManagerController {
+	
+	@Autowired
+    private EmployeeService employeeService;
+	
+	@RequestMapping(value="addMovie", method = RequestMethod.GET)
+	   public @ResponseBody void addMovie() {
+	 	 
+	   }
 
 }
